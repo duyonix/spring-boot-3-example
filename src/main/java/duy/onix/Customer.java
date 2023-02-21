2f8ce4dc27a112a619e57e892f5a8ed380de0ce1
duydt <duydt@vng.com.vn>
2023-02-21 17:33:20 +0700

    initialize source

---
+++ src/main/java/duy/onix/Customer.java
@@ -0,0 +1,81 @@
+package duy.onix;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name="customer_id_sequence",
+            sequenceName="customer_id_sequence")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                "}";
+    }
+}
---
+++ src/main/java/duy/onix/Main.java
@@ -0,0 +1,46 @@
+package duy.onix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse("Hello",
+                    List.of("Java", "Golang", "Javascript"),
+                    new Person("Onix", 10, 30_000)
+                );
+    }
+
+    record Person(String name, int age, double savings) {}
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {}
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//    }
+}
