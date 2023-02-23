3996546671d2d02792f8b9e01f154eefbbf3ce22
duyonix <dtduy2210@gmail.com>
2023-02-23 20:27:25 +0700

    update customer

--- src/main/java/duy/onix/CustomerRepository.java
+++ src/main/java/duy/onix/CustomerRepository.java
@@ -3,4 +3,5 @@ package duy.onix;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    
 }
--- src/main/java/duy/onix/Main.java
+++ src/main/java/duy/onix/Main.java
@@ -2,45 +2,54 @@ package duy.onix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse("Hello",
-                    List.of("Java", "Golang", "Javascript"),
-                    new Person("Onix", 10, 30_000)
-                );
+    @GetMapping
+    public List<Customer> list() {
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings) {}
-    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {}
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {}
 
-//    class GreetResponse {
-//        private final String greet;
-//
-//        GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet() {
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//    }
+    @PostMapping
+    public Customer addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        return customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public Customer updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request) {
+        Customer customer = customerRepository.findById(id).orElseThrow();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        return customerRepository.save(customer);
+    }
 }
